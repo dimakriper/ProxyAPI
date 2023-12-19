6310968881132a281ab5b4b416bba86421337516
= <=>
2023-12-19 10:36:02 +0300

    happy happy happy

--- Program.cs
+++ Program.cs
@@ -2,12 +2,15 @@ using ProxyAPI;
 
 var builder = WebApplication.CreateBuilder(args);
 
-
-
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    var xmlFile = "docs.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    c.IncludeXmlComments(xmlPath);
+});
 
 var app = builder.Build();
 
@@ -21,5 +24,41 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 
+app.MapGet("/companies/feed", () =>
+{
+var forecast = new FeedCompany
+    (
+        
+        
+    );
+        
+    return forecast;
+})
+.WithName("GetWeatherForecast");
+
 app.Run();
 
+internal record WeatherForecast()
+{
+
+    /// <summary>
+    /// Уникальный идентификатор организации
+    /// </summary>
+    [Newtonsoft.Json.JsonProperty("id", Required = Newtonsoft.Json.Required.Always)]
+    [System.ComponentModel.DataAnnotations.Required(AllowEmptyStrings = true)]
+    public string Id { get; set; }
+
+    /// <summary>
+    /// Название организации
+    /// </summary>
+    [Newtonsoft.Json.JsonProperty("name", Required = Newtonsoft.Json.Required.Always)]
+    [System.ComponentModel.DataAnnotations.Required(AllowEmptyStrings = true)]
+    public string Name { get; set; }
+
+    /// <summary>
+    /// Полный адрес
+    /// </summary>
+    [Newtonsoft.Json.JsonProperty("address", Required = Newtonsoft.Json.Required.Always)]
+    [System.ComponentModel.DataAnnotations.Required(AllowEmptyStrings = true)]
+    public string Address { get; set; }
+}
